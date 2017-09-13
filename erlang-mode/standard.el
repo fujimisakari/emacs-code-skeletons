d6a83b950fd899ff745c51393e0033ced3671e84
Fujimisakari <fujimisakari@gmail.com>
2017-09-13 09:13:54 +0900

    add erlang-mode/standard.el

---
+++ erlang-mode/standard.el
@@ -0,0 +1,29 @@
+;; standard.el --- Skeleton for insert current language element
+
+;; Copyright (C) 2017 by Ryo Fujimoto
+
+;; Author: Ryo Fujimoto <fujimisakri@gmail.com>
+;; URL: https://github.com/fujimisakari/emacs-code-skeleton
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(require 'skeleton)
+
+(define-skeleton debug-io:format
+  "Insert a debug statement."
+  ""
+  '(setq val (skeleton-read "Value? "))
+  > "io:format(\"*** debug message: ~p~n\", [" val "]),")
